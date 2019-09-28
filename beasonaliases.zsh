d507edfb1e2196ffe1785f11b6c388aafbba9971
Chris Beason <allmightyroot@gmail.com>
2019-09-28 17:59:43 -0500

    Adding gdf alias

--- beasonaliases.zsh
+++ beasonaliases.zsh
@@ -2,6 +2,7 @@
 alias ssh="ssh -q"
 alias ls="gls -h --color=auto"
 alias docker="sudo docker"
+alias df="gdf"
 
 # Misc
 alias geturl='wget -r --no-clobber --page-requisites --html-extension --convert-links'
